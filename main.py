946c7430fb42ce86b837ddf4304ddef733acd10f
Demo User <demouser@MacBook-Air.local>
2017-07-31 10:42:39 -0400

    Added sign in handler

--- main.py
+++ main.py
@@ -4,13 +4,26 @@ import urllib2
 import urllib
 import jinja2
 import os
+from google.appengine.api import users
 
 jinja_environment = jinja2.Environment(loader=
     jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
+                (user.nickname(), users.create_logout_url('/')))
+        else:
+            greeting = ('<a href="%s">Sign in or register</a>.' %
+                users.create_login_url('/'))
+
+        self.response.write('<html><body>%s</body></html>' % greeting)
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        template = jinja_environment.get_template("")
+        template = jinja_environment.get_template("templates/food.html")
         self.response.write(template.render())
 
     def post(self):
