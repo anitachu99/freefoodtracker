8c8b2e066827a7347472498a5e1625d5409d9011
arorasomya <arorasomya64@gmail.com>
2017-08-01 11:07:40 -0400

    figuring out the menu thing

--- static_folder/menu.js
+++ static_folder/menu.js
@@ -1,11 +1,34 @@
-function w3_open() {
-  document.getElementById("main").style.marginLeft = "25%";
-  document.getElementById("mySideBar").style.width = "25%";
-  document.getElementById("mySideBar").style.display = "block";
-  document.getElementById("openNav").style.display = 'none';
+// function w3_open() {
+//   document.getElementById("main").style.marginLeft = "25%";
+//   document.getElementById("mySideBar").style.width = "25%";
+//   document.getElementById("mySideBar").style.display = "block";
+//   document.getElementById("openNav").style.display = 'none';
+// }
+// function w3_close() {
+//   document.getElementById("main").style.marginLeft = "0%";
+//   document.getElementById("mySideBar").style.display = "none";
+//   document.getElementById("openNav").style.display = "inline-block";
+// }
+
+var open=false;
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    open=true;
 }
-function w3_close() {
-  document.getElementById("main").style.marginLeft = "0%";
-  document.getElementById("mySideBar").style.display = "none";
-  document.getElementById("openNav").style.display = "inline-block";
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    open=false;
+}
+
+function testTrue(){
+    if (open == true){
+      closeNav();
+    }
+    else if (open == false){
+      openNav();
+    }
 }
