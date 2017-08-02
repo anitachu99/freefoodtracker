1008a005129f8336c05f5f37f9a42afbf0433080
Demo User <demouser@MacBook-Air.local>
2017-08-02 15:22:35 -0400

    changes

---
+++ static_folder/allposts.js
@@ -0,0 +1,27 @@
+$("#menu").click(function() {
+  console.log("menu is clicked")
+  testTrue();
+});
+
+var open=false;
+
+function openNav() {
+    document.getElementById("mySideBar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    open=true;
+}
+
+function closeNav() {
+    document.getElementById("mySideBar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    open=false;
+}
+
+function testTrue(){
+    if (open == true){
+      closeNav();
+    }
+    else if (open == false){
+      openNav();
+    }
+}
