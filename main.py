434b2fbb0710f2b476c5e96fbf366694e429d47e
Demo User <demouser@MacBook-Air.local>
2017-08-02 16:40:13 -0400

    removed delete post handler

--- main.py
+++ main.py
@@ -109,55 +109,6 @@ class AddPostHandler(webapp2.RequestHandler):
         template = jinja_environment.get_template('templates/allposts.html')
         self.response.write(template.render(template_vars))
 
-class DeletePostHandler(webapp2.RequestHandler):
-    def get(self):
-        template = jinja_environment.get_template("templates/deletepost.html")
-        self.response.write(template.render())
-
-class ListPostHandler(webapp2.RequestHandler):
-    def get(self):
-        template = jinja_environment.get_template('templates/searchposts.html')
-        self.response.out.write(template.render())
-
-    def post(self):
-        userInput = self.request.get("location")
-        results = Food.query().fetch()
-        template_vars = {
-                'location': userInput,
-                'post': []
-                }
-        for result in results:
-            print result.location
-            print userInput.upper()
-            if result.location.upper() == userInput.upper():
-                    template_vars['post'].append(result)
-
-        print template_vars
-        # else:
-        #     self.response.out.write("We don't have that type of food yet. Sorry!")
-        template = jinja_environment.get_template('templates/listposts.html')
-        self.response.out.write(template.render(template_vars))
-
-class MenuHandler(webapp2.RequestHandler):
-    def get(self):
-        template = jinja_environment.get_template('templates/menu.html')
-        self.response.out.write(template.render())
-
-class AllPostHandler(webapp2.RequestHandler):
-    def get(self):
-        results = Food.query().order(Food.created).fetch()
-        template_vars = {
-                'post': []
-                }
-        for result in results:
-            template_vars['post'].append(result)
-
-        print template_vars
-        # else:
-        #     self.response.out.write("We don't have that type of food yet. Sorry!")
-        template = jinja_environment.get_template('templates/allposts.html')
-        self.response.out.write(template.render(template_vars))
-
 class CalendarHandler(webapp2.RequestHandler):
     def get(self):
         template = jinja_environment.get_template("templates/calendar.html")
