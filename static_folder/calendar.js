880edb870faef85911bb01245c5d9bca86faaf34
arorasomya <arorasomya64@gmail.com>
2017-08-03 11:10:46 -0400

    finished the calendar yipeeegit add .

--- static_folder/calendar.js
+++ static_folder/calendar.js
@@ -3,6 +3,570 @@ $( "#menu" ).click(function() {
   testTrue();
 });
 
+$('#jan').click(function() {
+  $('#monthy').empty();
+  $('#monthy').append("January");
+  $('.days').empty();
+  var year = "2017";
+  var month = "1";
+  var day = new Date(year + "-" + month + "-01").getDay()
+  var numdays = daysInMonth(month, year)
+  if (day==0) {
+    putDaysIn(numdays);
+  } else if (day==1) {
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==2) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==3) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==4) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==5) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==6) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  }
+})
+
+$('#feb').click(function() {
+  $('#monthy').empty();
+  $('#monthy').append("February");
+  $('.days').empty();
+  var year = "2017";
+  var month = "2";
+  var day = new Date(year + "-" + month + "-01").getDay()
+  var numdays = daysInMonth(month, year)
+  if (day==0) {
+    putDaysIn(numdays);
+  } else if (day==1) {
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==2) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==3) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==4) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==5) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==6) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  }
+})
+
+$('#mar').click(function() {
+  $('#monthy').empty();
+  $('#monthy').append("March");
+  $('.days').empty();
+  var year = "2017";
+  var month = "3";
+  var day = new Date(year + "-" + month + "-01").getDay()
+  var numdays = daysInMonth(month, year)
+  if (day==0) {
+    putDaysIn(numdays);
+  } else if (day==1) {
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==2) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==3) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==4) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==5) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==6) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  }
+})
+
+$('#apr').click(function() {
+  $('#monthy').empty();
+  $('#monthy').append("April");
+  $('.days').empty();
+  var year = "2017";
+  var month = "4";
+  var day = new Date(year + "-" + month + "-01").getDay()
+  var numdays = daysInMonth(month, year)
+  if (day==0) {
+    putDaysIn(numdays);
+  } else if (day==1) {
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==2) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==3) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==4) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==5) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==6) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  }
+})
+
+$('#may').click(function() {
+  $('#monthy').empty();
+  $('#monthy').append("May");
+  $('.days').empty();
+  var year = "2017";
+  var month = "5";
+  var day = new Date(year + "-" + month + "-01").getDay()
+  var numdays = daysInMonth(month, year)
+  if (day==0) {
+    putDaysIn(numdays);
+  } else if (day==1) {
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==2) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==3) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==4) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==5) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==6) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  }
+})
+
+$('#jun').click(function() {
+  $('#monthy').empty();
+  $('#monthy').append("June");
+  $('.days').empty();
+  var year = "2017";
+  var month = "6";
+  var day = new Date(year + "-" + month + "-01").getDay()
+  var numdays = daysInMonth(month, year)
+  if (day==0) {
+    putDaysIn(numdays);
+  } else if (day==1) {
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==2) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==3) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==4) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==5) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==6) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  }
+})
+
+$('#jul').click(function() {
+  $('#monthy').empty();
+  $('#monthy').append("July");
+  $('.days').empty();
+  var year = "2017";
+  var month = "7";
+  var day = new Date(year + "-" + month + "-01").getDay()
+  var numdays = daysInMonth(month, year)
+  if (day==0) {
+    putDaysIn(numdays);
+  } else if (day==1) {
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==2) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==3) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==4) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==5) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==6) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  }
+})
+
+$('#aug').click(function() {
+  $('#monthy').empty();
+  $('#monthy').append("August");;
+  $('.days').empty();
+  var year = "2017";
+  var month = "8";
+  var day = new Date(year + "-" + month + "-01").getDay()
+  var numdays = daysInMonth(month, year)
+  if (day==0) {
+    putDaysIn(numdays);
+  } else if (day==1) {
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==2) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==3) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==4) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==5) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==6) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  }
+})
+
+$('#sep').click(function() {
+  $('#monthy').empty();
+  $('#monthy').append("September");
+  $('.days').empty();
+  var year = "2017";
+  var month = "9";
+  var day = new Date(year + "-" + month + "-01").getDay()
+  var numdays = daysInMonth(month, year)
+  if (day==0) {
+    putDaysIn(numdays);
+  } else if (day==1) {
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==2) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==3) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==4) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==5) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==6) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  }
+})
+
+$('#oct').click(function() {
+  $('#monthy').empty();
+  $('#monthy').append("October");
+  $('.days').empty();
+  var year = "2017";
+  var month = "10";
+  var day = new Date(year + "-" + month + "-01").getDay()
+  var numdays = daysInMonth(month, year)
+  if (day==0) {
+    putDaysIn(numdays);
+  } else if (day==1) {
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==2) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==3) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==4) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==5) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==6) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  }
+})
+
+$('#nov').click(function() {
+  $('#monthy').empty();
+  $('#monthy').append("November");
+  $('.days').empty();
+  var year = "2017";
+  var month = "11";
+  var day = new Date(year + "-" + month + "-01").getDay()
+  var numdays = daysInMonth(month, year)
+  if (day==0) {
+    putDaysIn(numdays);
+  } else if (day==1) {
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==2) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==3) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==4) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==5) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==6) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  }
+})
+
+$('#dec').click(function() {
+  $('#monthy').empty();
+  $('#monthy').append("December");
+  $('.days').empty();
+  var year = "2017";
+  var month = "12";
+  var day = new Date(year + "-" + month + "-01").getDay()
+  var numdays = daysInMonth(month, year)
+  if (day==0) {
+    putDaysIn(numdays);
+  } else if (day==1) {
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==2) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==3) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==4) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==5) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  } else if (day==6) {
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    $('.days').append("<li></li>");
+    putDaysIn(numdays);
+  }
+})
+
+
+
+function putDaysIn(numdays) {
+  for (i=1;i<numdays+1;i++) {
+    $('.days').append("<li>"+i+"</li>");
+  }
+}
+
+function daysInMonth(month,year) {
+    return new Date(year, month, 0).getDate();
+}
+
 var open=false;
 
 function openNav() {
